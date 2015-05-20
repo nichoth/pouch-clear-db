bf595dd0309a295c398e3d038fc50b935e8ece54
nichoth <nichoth@gmail.com>
2015-05-20 16:05:27 -0700

    return promise

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = function clearDb(db) {
-  db.allDocs({include_docs: true}).then(function(resp) {
-    var docs = resp.rows.map(function(row) {
-      row.doc._deleted = true;
-      return row.doc;
-    });
-    return db.bulkDocs(docs);
-  });
+  return db.allDocs({include_docs: true})
+    .then(function(resp) {
+
+      var docs = resp.rows.map(function(row) {
+        row.doc._deleted = true;
+        return row.doc;
+      });
+
+      return db.bulkDocs(docs);
+    }).then(function() {
+      return db.compact();
+    })
+  ;
 };
